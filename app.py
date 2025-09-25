df3c447f72ce381485c9e8d7b6af5cefcf993d49
Jordan Freedman <115061495+jordan-f-21@users.noreply.github.com>
2025-09-25 14:51:05 -0700

    new app.py

--- app.py
+++ app.py
@@ -1,57 +1,20 @@
-import requests
-import json
-import statsapi
 import mlbstatsapi
 
 mlb = mlbstatsapi.Mlb()
 
-# Example: Get Dodgers roster
-#wow = statsapi.get("sports_players")['season']
+def getId(name):
+    return mlb.get_people_id(name)[0]
 
+#wow = mlb.get_person(getId("Mike Trout"))
 
-#url = "http://statsapi.mlb.com/api/v1/"
-#response = requests.get(url)
-#ata = response.json()
-#rint(json.dumps(data, indent=4))
+player_id = mlb.get_people_id('Mike Trout')[0]
 
-#stats = ['expectedStatistics']
-#groups = ['hitting']
-#arams = {'season': 2022}
-
-#def getId(name):
-#    return mlb.get_people_id(name)[0]
-
-#print(wow)
-#print(dir(mlb))
-#mmm = mlb.get_player_stats(getId("Mike Trout"), stats=stats, groups=groups, **params)
-#mmm2 = mmm['expectedStatistics']['season']
-#for split in mmm2.splits:
-#     for k, v in split.stat.__dict__.items():
-#            print(k, v)
-
-player_id = mlb.get_people_id('Ty France')[0]
-
-stats = ['expectedStatistics']
+stats = ['season','career']
 group = ['hitting']
 params = {'season': 2022}
 
-stats = mlb.get_player_stats(player_id, stats=stats, groups=group, **params)
-expectedstats = stats['hitting']['expectedstatistics']
-for split in expectedstats.splits:
+stat_dict = mlb.get_player_stats(player_id, stats=['season'], groups=['hitting'], season=2025)
+season_hitting_stat = stat_dict['hitting']['season']
+for split in season_hitting_stat.splits:
     for k, v in split.stat.__dict__.items():
         print(k, v)
-
-
-
-#print(data)
-
-#roster = data["roster"]
-
-
-#for x in data:
-    #name = player["person"]["fullName"]
-    #jersey = player.get("jerseyNumber", "N/A")
-    #position = player["position"]["name"]
-    #status = player["status"]["description"]
-    #print(f"{name:<20} | #{jersey:<3} | {position:<15} | {status}")
-    #print(x)
